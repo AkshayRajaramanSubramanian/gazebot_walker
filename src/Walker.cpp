8eae00b29786ef69dd5bd22f06a7b97f94d39d3b
Akshay Rajaraman Subramanian <akshayrs@terpmail.umd.edu>
2018-11-20 19:15:49 -0500

    added doxygen comments and license information

--- src/Walker.cpp
+++ src/Walker.cpp
@@ -1,8 +1,47 @@
-#include "../include/gazebot_walker/Walker.h"
+/*
+ * @file Walker.cpp
+ * @Copyright MIT license
+ * Copyright (c) 2018 Akshay Rajaraman
+ * @author Akshay Rajaraman
+ * @brief class that describes the function of the walker node
+ *
+ */
 
+
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Akshay Rajaraman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "../include/gazebot_walker/Walker.h"
 #include <cmath>
 #include <limits>
 #include <vector>
+#include <utility>
+/**
+ * @brief constructor for initializing Walker object
+ * @param NodeHandle n
+ */
 
 Walker::Walker(ros::NodeHandle &n) : n(n) {
   publisher =
@@ -11,7 +50,9 @@ Walker::Walker(ros::NodeHandle &n) : n(n) {
 }
 
 Walker::~Walker() {}
-
+/**
+ * @brief callback function that runs when data is being subscribed from the robot
+ */
 void Walker::getLaserData(const sensor_msgs::LaserScan::ConstPtr &scanData) {
   std::vector<float> laserData = scanData->ranges;
   auto angleRes = scanData->angle_increment;
@@ -40,7 +81,9 @@ void Walker::getLaserData(const sensor_msgs::LaserScan::ConstPtr &scanData) {
   }
   move(isObstacle);
 }
-
+/**
+ * @brief function that moves the robot either linearly or angularly
+ */
 void Walker::move(bool isObstacle) {
   if (isObstacle) {
     msg.linear.x = 0.0;
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,47 @@
+/*
+ * @file main.cpp
+ * @Copyright MIT license
+ * Copyright (c) 2018 Akshay Rajaraman
+ * @author Akshay Rajaraman
+ * @brief Node that establishes a simple walker behavior in the turtlebot v2
+ *
+ */
+
+
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Akshay Rajaraman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <ros/console.h>
 #include "../include/gazebot_walker/Walker.h"
+
+/**
+ * @brief function that initializes and starts the Walker object
+ */
 int main(int argc, char **argv) {
   ros::init(argc, argv, "walker");
   ros::NodeHandle nh;
-  if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME,
-                                     ros::console::levels::Debug)) {
-    ros::console::notifyLoggerLevelsChanged();
-  }
   Walker walker(nh);
   ros::spin();
   return 0;
